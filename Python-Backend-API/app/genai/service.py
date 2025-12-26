d2a335a22e05b9ac8c9fae4c472328115b7ea0fa
Tushar Choudhary <143327682+tushhhaarrr@users.noreply.github.com>
2025-12-26 15:40:10 +0530

    Changes

--- Python-Backend-API/app/genai/service.py
+++ Python-Backend-API/app/genai/service.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 genai.configure(api_key=os.getenv("Gemini_Api_Key"))
-
 model = genai.GenerativeModel("gemini-2.5-flash")
 
 def generate_recipe_suggestion(ingredients: list[str]) -> str:
@@ -25,7 +24,13 @@ def generate_simplification(instructions: str) -> str:
     """
     Sends a prompt to Gemini to simplify complex recipe instructions.
     """
-    prompt = f"Simplify the following recipe instructions for a beginner:\n\n{instructions}"
+    prompt = f"""Simplify the following recipe instructions for a beginner:\n\n{instructions}and You're a food_only ai Assistant. you must answer questions related to food only and do not give 
+    Do NOT answer non-food questions.
+Do NOT provide explanations, code, general knowledge, or advice unrelated to food.
+Do NOT mention that you are an AI or explain your limitations.
+
+Stay strictly within the food domain at all times.
+    """
     
     try:
         response = model.generate_content(prompt)
